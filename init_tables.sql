b00fd28a1252e16b962b0eb3a542e7f96d9b525d
Amit Nagdev <amit@nagdev.com>
2018-03-03 21:32:22 -0800

    adding basic tables.
    May update in future

---
+++ init_tables.sql
@@ -0,0 +1,28 @@
+CREATE TABLE DisasterList (
+      ID    NUMBER(38) PRIMARY KEY,
+      Name VARCHAR(35) UNIQUE,
+      nCommentID NUMBER,
+      nPictureSetID NUMBER,
+      nPictureID NUMBER
+);
+
+CREATE TABLE Post (
+  CommentID NUMBER(38) PRIMARY KEY,
+  Title VARCHAR(35),
+  Category VARCHAR(35),
+  Subcategory VARCHAR (35),
+  tag VARCHAR (35),
+  PostText VARCHAR(1000),
+  PictureSetID NUMBER(38),
+  UserID NUMBER(38),
+  GeoLocation VARCHAR (21),
+  Upvotes NUMBER (5),
+  Downvotes NUMBER (5),
+  MyTime TIMESTAMP
+);
+
+CREATE TABLE Pictures (
+  ID NUMBER(38) PRIMARY KEY,
+  SetID NUMBER (38),
+  image BLOB
+);
